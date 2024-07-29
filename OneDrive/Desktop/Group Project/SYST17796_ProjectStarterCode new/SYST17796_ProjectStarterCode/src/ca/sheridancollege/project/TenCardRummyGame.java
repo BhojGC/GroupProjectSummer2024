644777f673dbfdf3d8f9fcf4b4f937eed68ae94e
BhojGC <ghartich@sheridancollege.ca>
2024-07-29 17:11:46 -0400

    version1- methods addedd

---
+++ OneDrive/Desktop/Group Project/SYST17796_ProjectStarterCode new/SYST17796_ProjectStarterCode/src/ca/sheridancollege/project/TenCardRummyGame.java
@@ -0,0 +1,61 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ca.sheridancollege.project;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author gcbho
+ */
+public class TenCardRummyGame extends Game {
+    private GroupOfCards deck;
+    private GroupOfCards discardPile;
+    private RummyPlayer player1;
+    private RummyPlayer player2;
+    private int player1Points;
+    private int player2Points;
+    
+    public TenCardRummyGame(String name){
+        super(name);
+        intiializeGame();
+    }
+    
+    public void intiializeGame(){
+    
+    
+}
+    
+public ArrayList<Card> createDeck(){
+    return null;
+}
+
+private void dealCards(){
+    
+}
+
+@Override
+public void play(){
+    
+}
+
+public boolean declareHand(RummyPlayer player){
+    return false;
+}
+
+public boolean verifyDeclaration(RummyPlayer player){
+    return false;
+}
+
+public void endRound(RummyPlayer player){
+    
+}
+
+@Override
+public void declareWinner(){
+    
+}
+    
+}
