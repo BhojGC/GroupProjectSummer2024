dd8fab17f7e608f237ec0a5c008ea97d65d3643a
BhojGC <ghartich@sheridancollege.ca>
2024-08-02 12:58:53 -0400

    version 23 editing in puresequence method

--- OneDrive/Desktop/Group Project Final/SYST17796_ProjectStarterCode/SYST17796_ProjectStarterCode/src/ca/sheridancollege/project/Hand.java
+++ OneDrive/Desktop/Group Project Final/SYST17796_ProjectStarterCode/SYST17796_ProjectStarterCode/src/ca/sheridancollege/project/Hand.java
@@ -4,17 +4,24 @@
  */
 package ca.sheridancollege.project;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 /**
  *
  * @author gcbho
  */
 public class Hand extends GroupOfCards {
+    
+    private List<Card> cards;
+    private List<Value> value;
+    
     public Hand(){
         super(0);
-        //testing git
-       // this.setCard(new ArrayList<>());
+        
     }
     
     public void addCard(Card card){
@@ -27,9 +34,50 @@ public class Hand extends GroupOfCards {
     }
     
 public List<Card> getPureSequence(){
-    
-    return null;
-}
+    List<Card> pureSequences = new ArrayList<>();
+    //Group cards by suit
+    Map<Suit, List<Card>> cardsBySuit = cards.stream()
+                .collect(Collectors.groupingBy(Card::getSuit));
+    //Iterating through each suit to find sequence
+        for (Map.Entry<Suit, List<Card>> entry : cardsBySuit.entrySet()) {
+            Suit suit = entry.getKey();
+            List<Card> suitCards = entry.getValue();
+
+            // Sort the cards in ascending order by their value
+            Collections.sort(suitCards, (c1, c2) -> Integer.compare(c1.getValue().getPoints(), c2.getValue().getPoints()));
+
+            // Find sequences within the sorted list
+            List<Card> currentSequence = new ArrayList<>();
+            for (int i = 0; i < suitCards.size(); i++) {
+                if (currentSequence.isEmpty()) {
+                    currentSequence.add(suitCards.get(i));
+                } else {
+                    Card lastCard = currentSequence.get(currentSequence.size() - 1);
+                    Card currentCard = suitCards.get(i);
+
+                    // Check if the current card is the next consecutive value
+                    if (currentCard.getValue().getPoints() == lastCard.getValue().getPoints() + 1) {
+                        currentSequence.add(currentCard);
+                    } else {
+                        // If sequence breaks, check if it is valid and reset
+                        if (currentSequence.size() >= 3) {
+                            pureSequences.addAll(currentSequence);
+                        }
+                        currentSequence.clear();
+                        currentSequence.add(currentCard);
+                    }
+                }
+            }
+
+            // Check the last sequence
+            if (currentSequence.size() >= 3) {
+                pureSequences.addAll(currentSequence);
+            }
+        }
+
+        return pureSequences;
+    }
+
 
 public List<Card> getImpureSequence(){
     
