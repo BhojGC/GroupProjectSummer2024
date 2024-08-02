9c68e12999674d1be20a8930f2c5207d9ef41bcd
BhojGC <ghartich@sheridancollege.ca>
2024-08-02 09:46:17 -0400

    version 9 deck creation

--- OneDrive/Desktop/Group Project Final/SYST17796_ProjectStarterCode/SYST17796_ProjectStarterCode/src/ca/sheridancollege/project/TenCardRummyGame.java
+++ OneDrive/Desktop/Group Project Final/SYST17796_ProjectStarterCode/SYST17796_ProjectStarterCode/src/ca/sheridancollege/project/TenCardRummyGame.java
@@ -20,21 +20,48 @@ public class TenCardRummyGame extends Game{
     
     public TenCardRummyGame(String name){
         super(name);
-        intiializeGame();
+        intializeGame();
     }
     
     public void intiializeGame(){
+        //Initialize deck with 52 cards
+        deck = new GroupOfCards(createDeck());
+        //Initialize discard pile
+        discardPile = new GroupOfCards(new ArrayList<>());
+        
+        //Initialize Players
+        player1 = new RummyPlayer(player1.getName());
+        player2 = new RummyPlayer(player2.getName());
+        
+        //Deal cards to players
+        dealcards(player1, player2);
+        
+        
     
     
 }
     
-public ArrayList<Card> createDeck(){
-    return null;
+public ArrayList<PlayingCard> createDeck(){
+ ArrayList<PlayingCard> deck = new ArrayList<>();
+    for(Suit suit : Suit.values()){
+        for(Value value : Value.values()){
+            deck.add(new PlayingCard(suit, value));
+        }
+    }
+    return deck;
 }
 
-private void dealCards(){
-    
-}
+private void dealCards(RummyPlayer player1, RummyPlayer player2) {
+        for (int i = 0; i < deck.getCards().size(); i++) {
+            PlayingCard card = deck.getCards().get(i);
+
+            if (i % 2 != 0) { // Odd index - deal to player1
+                player1.getHand().addCard(card);
+            } else { // Even index - deal to player2
+                player2.getHand().addCard(card);
+            }
+        }
+    }
 
 @Override
 public void play(){
